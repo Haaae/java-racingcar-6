696f167b81dc30087afdf2b759aba891729e7e58
김민규 <alsrbtls88@gmail.com>
2023-11-24 11:55:34 +0900

    dir: 이동 메서드 및 getter 구현

--- src/main/java/racingcar/entity/car/Car.java
+++ src/main/java/racingcar/entity/car/Car.java
@@ -3,26 +3,45 @@ package racingcar.entity.car;
 import java.util.Objects;
 import racingcar.entity.Entity;
 import racingcar.exception.Exception;
+import racingcar.service.RaceNumber;
 import racingcar.validator.Validator;
 
 public class Car extends Entity {
 
     public static final int MAXIMUM_NAME_SIZE = 5;
     public static final int MINIMUM_NAME_SIZE = 1;
+    public static final int MOVE_DISTANCE = 1;
 
     private final Long id;
     private final String name;
+    private long distance;
 
     public Car(final String name, final Validator validator) {
         validator.isValidSize(name, MAXIMUM_NAME_SIZE, MINIMUM_NAME_SIZE, Exception.INVALID_INPUT);
 
         this.name = name;
         this.id = getNextId();
+        this.distance = 0L;
+    }
+
+    public long move(final RaceNumber number) {
+        if (number.canMove()) {
+            this.distance += MOVE_DISTANCE;
+        }
+        return this.distance;
     }
 
     @Override
     public Long getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getDistance() {
+        return distance;
     }
 
     @Override
@@ -41,4 +60,5 @@ public class Car extends Entity {
     public int hashCode() {
         return Objects.hash(id);
     }
+
 }
